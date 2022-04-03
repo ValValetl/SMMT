139652348dacb02dee4b1c25681d3405f900ff1b
Valentin Knechtl <valentinknechtl@gmail.com>
2022-04-03 11:44:32 +0200

    update

--- R/get_current_url.R
+++ R/get_current_url.R
@@ -6,20 +6,17 @@
 
 get_current_url <- function() {
   
-  base_url <- "https://www.bfs.admin.ch"
-  url_xml_link <- file.path(base_url, "bfs/de/home/statistiken/kataloge-datenbanken.assetdetail.dz-b-00.04-hgv-02.html")
+  url_xml_link <- file.path("https://www.bfs.admin.ch", 
+                            "bfs/de/home/statistiken/kataloge-datenbanken.assetdetail.dz-b-00.04-hgv-02.html")
   
   hp_object <- read_html(url_xml_link)
-  path <- html_nodes(hp_object,"a") %>% html_attr("href")
-  path <- path[grep("assets", path, perl = FALSE)]
-  path <- unique(path[grep("master", path, perl = FALSE)])
+  current_url <- html_nodes(hp_object,"a") %>% html_attr("href")
+  current_url <- current_url[grep("assets", current_url, perl = FALSE)]
+  current_url <- unique(current_url[grep("master", current_url, perl = FALSE)])
   
-  if (length(path) > 1) {
+  if (length(current_url) > 1) {
     stop("error: Link detection failed.")
   }
   
-  current_url <- paste0(base_url, path)
-  
   return(current_url)
 }
-
