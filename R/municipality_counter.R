694ade9e98c6ae8152892fb917b2ff0745b92300
ValValetl <74460707+ValValetl@users.noreply.github.com>
2023-04-01 07:45:35 +0200

    add possilibity to count with lakes

--- R/municipality_counter.R
+++ R/municipality_counter.R
@@ -1,7 +1,7 @@
 #' Municipality counter
 #'
 #' Count the municipalities for a set of dates. Either at the national or
-#' cantonal level. See vignette for details.
+#' cantonal level.  See vignette for details.
 #'
 #' @param mutations A tibble containing the municipality mutations inventory
 #'   (see \code{\link{import_CH_municipality_inventory}})
@@ -11,23 +11,31 @@
 #' @return A tibble with the municipality count per date and specified
 #'   geography.
 #'
+#' @note All entities that have a bfs nr are counted (e.g. also Gemeindefreie
+#'   Gebiete). This is not exactly what the BfS does in the webtool Applikation
+#'   der Schweizer Gemeinden.  However, it is not possible to distinguish
+#'   "Gemeinden" und "Gemeindefreie Gebiete" generically, based on the
+#'   information in the Gemeindeverzeichnis.
+#'
 #' @export
 
-municipality_counter <- function(mutations, dates, geo_level = "ch") {
+municipality_counter <- function(mutations, dates, geo_level = "ch", include_cant_lakes = FALSE) {
   
-  mutations_no_lakes <- filter(mutations, !(bfs_nr >= 9000 & bfs_nr < 10000))
+  if (!include_cant_lakes) {
+    mutations <- filter(mutations, !(bfs_nr >= 9000 & bfs_nr < 10000))
+  }
   
   if (geo_level == "ch") {
     result <- rep(NA, length(dates))  
     for (ii in 1:length(dates)) {
-      result[ii] <- nrow(filter_date(mutations_no_lakes, date = dates[ii]))
+      result[ii] <- nrow(filter_date(mutations, date = dates[ii]))
     }
     t_result <- tibble(date = dates, n = result)
   } else if (geo_level == "cantons") {
     
     result <- list()
     for (ii in 1:length(dates)) {
-      result[[ii]] <- filter_date(mutations_no_lakes, date = dates[ii]) %>% 
+      result[[ii]] <- filter_date(mutations, date = dates[ii]) %>% 
         group_by(canton) %>% 
         summarise(n = n()) %>% add_column(date = dates[ii], .before = 1)
     }
